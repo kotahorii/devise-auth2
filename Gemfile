3153f1044e9f71a34c60f37b2aa3fdd85c13ccb6
kotahorii <khorii.micron@gmail.com>
2021-11-14 16:42:10 +0900

    posts controller created

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'devise_token_auth'
 gem 'active_model_serializers'
+gem 'activerecord-session_store'
---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :authenticate_api_v1_user!
+  before_action :set_post, except:[:index, :create]
+
+  def index
+    posts = Post.all
+    render json: { data: posts }
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    user = current_api_v1_user
+    post = user.posts.create(
+      text: post_params[:text],
+      user_id: user.id
+    )
+    if post
+      render json: { data: post }
+    else
+      render status: 400, json: { data: post.errors }
+    end
+  end
+
+  def update
+    user = current_api_v1_user
+    if @post.update(
+      text: post_params[:text],
+      user_id: user.id
+    )
+      render json: { data: @post }
+    else
+      render status: 400, json: { data: @post.errors }
+    end
+  end
+
+  def destroy
+    @post.destroy
+    render status: 200, json: { msg: '投稿を削除しました。' }
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(params[:id])
+  end
+
+  def post_params
+    params.permit(:text)
+  end
+end
